265b58641ca1073e21c445cb1a6773f0c7e862b4
Lucas César <lucasambrogi1@hotmail.com>
2020-12-21 23:59:15 -0300

    Error Handler middleware: created.

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import express from "express";
 import 'express-async-errors';
 
 import routes from './routes';
+import ErrorHandler from './middlewares/ErrorHandler';
 
 import './database';
 import './container';
@@ -15,6 +16,7 @@ app.use(express.json());
 
 app.use(routes);
 
+app.use(ErrorHandler);
 
 app.listen(3333, () => {
     console.log('Server listening on port 3333!')
